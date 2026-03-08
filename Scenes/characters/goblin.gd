2b3c8f0ec739e1d73b2932d2f5ebaa47038e8bbf
kstates <kathleen.states@gmail.com>
2026-03-08 14:23:14 -0700

    feat: Starting on types of goblins and UI to switch between types

--- Scenes/characters/goblin.gd
+++ Scenes/characters/goblin.gd
@@ -2,19 +2,28 @@ extends CharacterBody2D
 
 var direction_x := 1.0 
 var speed := 5000
+var character_type: Characters = Characters.STONER
 @export var gravity := 40
+
+enum Characters {BASIC, STONER}
+const character_connection = {
+	Characters.BASIC: 'basic',
+	Characters.STONER: 'stoner'
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	# TODO: I don't think this is where I want to do this
+	if character_type == Characters.STONER:
+		set_collision_mask_value(2, true)
 
 func _physics_process(delta: float) -> void:
 	apply_gravity(delta)
 	ambulate(delta)
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		if (collision.get_collider().is_in_group("Obstacles")):
+		var collidor = collision.get_collider()
+		if (collidor.has_method('is_obstacle') && collidor.is_obstacle()):
 			direction_x = direction_x * -1.0
 		else: 
 			velocity = velocity.slide(collision.get_normal())
@@ -24,3 +33,6 @@ func ambulate(delta: float) -> void:
 	
 func apply_gravity(delta: float) -> void:
 	velocity.y += gravity * delta
+	
+func is_obstacle() -> bool:
+	return character_type == Characters.STONER
--- Scenes/characters/obstacle.gd
+++ Scenes/characters/obstacle.gd
@@ -1,1 +1,4 @@
 extends StaticBody2D
+
+func is_obstacle() -> bool:
+	return true
