aaf504a157498400e5da494ba5df5f25936ef115
kstates <kathleen.states@gmail.com>
2026-03-14 10:14:59 -0700

    Adding temp goblin sprites and swapping them out on character type change

--- Scenes/characters/goblin.gd
+++ Scenes/characters/goblin.gd
@@ -3,7 +3,21 @@ extends CharacterBody2D
 var direction_x := 1.0 
 var speed := 5000
 var character_type: Constants.Characters
+var current_sprite: Texture
 @export var gravity := 40
+
+# Character Images
+var basic_goblin_sprite = preload("res://Images/TempImages/aseprite/basic-goblin-one.png")
+var stoner_goblin_sprite = preload("res://Images/TempImages/aseprite/stoner-gobline-one.png")
+
+var character_types_dict = {
+	Constants.Characters.BASIC: {
+		"image": basic_goblin_sprite,
+	},
+	Constants.Characters.STONER: {
+		"image": stoner_goblin_sprite
+	}
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,14 +25,15 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	apply_gravity(delta)
-	ambulate(delta)
-	var collision = move_and_collide(velocity * delta)
-	if collision:
-		var collidor = collision.get_collider()
-		if (collidor.has_method('is_obstacle') && collidor.is_obstacle()):
-			direction_x = direction_x * -1.0
-		else: 
-			velocity = velocity.slide(collision.get_normal())
+	if !is_obstacle():
+		ambulate(delta)
+		var collision = move_and_collide(velocity * delta)
+		if collision:
+			var collidor = collision.get_collider()
+			if (collidor.has_method('is_obstacle') && collidor.is_obstacle()):
+				direction_x = direction_x * -1.0
+			else: 
+				velocity = velocity.slide(collision.get_normal())
 	
 func ambulate(delta: float) -> void:
 	velocity.x = speed * delta * direction_x
@@ -31,9 +46,11 @@ func is_obstacle() -> bool:
 	
 func set_character_type(char_type: Constants.Characters):
 	character_type = char_type
-	if character_type == Constants.Characters.STONER:
-			set_collision_mask_value(2, true)
+	set_collision_layer_value(3, is_obstacle())
+	
+	$Sprite2D.texture = character_types_dict[character_type].image
 
+			
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("left click"):
 		for button in get_tree().get_nodes_in_group("characterassigners"):
